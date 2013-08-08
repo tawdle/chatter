b8bfccb104343048918078dda0f6e1530cc4c1a3
Todd Agulnick <todd@agulnick.com>
2013-08-08 10:04:38 -0700

    Initial commit

---
+++ chatter.js
@@ -0,0 +1,24 @@
+var http = require('http'),
+    faye = require('faye');
+
+var server = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+var secret = 'this_is_a_SeCRet';
+
+server.addExtension({
+  incoming: function(message, callback) {
+    if (!message.channel.match(/^\/meta\//)) {
+      var password = message.ext && message.ext.password;
+      if (password !== secret)
+        message.error = '403::Password required';
+    }
+    callback(message);
+  },
+
+  outgoing: function(message, callback) {
+    if (message.ext) delete message.ext.password;
+    callback(message);
+  }
+});
+
+server.listen(8000);
